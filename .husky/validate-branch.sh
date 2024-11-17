9223b9276c4f0b9470c6fcc3625b0744522c9351
Chung Tu <tuthanhtrung2000@gmail.com>
2024-11-18 00:19:59 +0700

    chore: refactor validate push

--- .husky/validate-branch.sh
+++ .husky/validate-branch.sh
@@ -6,23 +6,21 @@ PLATFORM=$(uname)
 # Get the local branch name
 local_branch_name="$(git rev-parse --abbrev-ref HEAD)"
 
-# Read metadata from files
+# Read metadata from the type file
 TYPE="$(cat "$PWD/.husky/metadata/type.txt")"
-SERVICE="$(cat "$PWD/.husky/metadata/service.txt")"
-ISSUE_CODE="$(cat "$PWD/.husky/metadata/issue_code.txt")"
 
-# Define the regex for a valid branch name
-valid_branch_regex="^(($TYPE)\(($SERVICE)\)\/($ISSUE_CODE(-)[0-9]+(-)|no-issue(-))[a-zA-Z0-9\-]+)$"
+# Define the regex for a valid branch name (simplified to (type)/message)
+valid_branch_regex="^($TYPE)\/[a-zA-Z0-9\-]+$"
 
 # Error message for invalid branch name
 message="❌ Branch name invalid, regex: $valid_branch_regex"
 
-# Use awk to check if the commit message matches the regex
+# Use awk to check if the branch name matches the regex
 if echo "$local_branch_name" | awk "/$valid_branch_regex/ { exit 0 } { exit 1 }"; then
   exit 0
 else
   echo "$message"
-  echo "Eg: feat($SERVICE)/$ISSUE_CODE-112-test-branch"
+  echo "Eg: feat/refactor-route"
   exit 1
 fi
 
